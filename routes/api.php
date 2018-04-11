6877cad10f2928a204cdea96990df1d82919c327
rob <oh.ya.rob@gmail.com>
2018-04-10 23:01:41 -0500

    more controllers

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,9 @@ use Illuminate\Http\Request;
  * * Buyers
  * */
 Route::resource('buyers', 'Buyer\BuyerController', ['only' => ['index', 'show']]);
+Route::resource('buyers.sellers', 'Buyer\BuyerSellerController', ['only' => ['index']]);
+Route::resource('buyers.products', 'Buyer\BuyerProductController', ['only' => ['index']]);
+Route::resource('buyers.transactions', 'Buyer\BuyerTransactionController', ['only' => ['index']]);
 
 /**
  * * Categories
@@ -37,6 +40,8 @@ Route::resource('sellers', 'Seller\SellerController', ['only' => ['index', 'show
  * * Transactions
  * */
 Route::resource('transactions', 'Transaction\TransactionController', ['only' => ['index', 'show']]);
+Route::resource('transactions.categories', 'Transaction\TransactionCategoryController', ['only' => ['index']]);
+Route::resource('transactions.sellers', 'Transaction\TransactionSellerController', ['only' => ['index']]);
 
 /**
  * * Users
