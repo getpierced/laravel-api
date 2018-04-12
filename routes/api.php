536c2101a8f50660a13a3a887a2a586055830bfe
rob <oh.ya.rob@gmail.com>
2018-04-12 00:01:29 -0500

    seller controller

--- routes/api.php
+++ routes/api.php
@@ -27,6 +27,9 @@ Route::resource('buyers.transactions', 'Buyer\BuyerTransactionController', ['onl
  * */
 Route::resource('categories', 'Category\CategoryController', ['except' => ['create', 'edit']]);
 Route::resource('categories.products', 'Category\CategoryProductController', ['only' => ['index']]);
+Route::resource('categories.transactions', 'Category\CategoryTransactionController', ['only' => ['index']]);
+Route::resource('categories.buyers', 'Category\CategoryBuyerController', ['only' => ['index']]);
+Route::resource('categories.sellers', 'Category\CategorySellerController', ['only' => ['index']]);
 
 /**
  * * Products
@@ -37,6 +40,10 @@ Route::resource('products', 'Product\ProductController', ['only' => ['index', 's
  * * Sellers
  * */
 Route::resource('sellers', 'Seller\SellerController', ['only' => ['index', 'show']]);
+Route::resource('sellers.transactions', 'Seller\SellerTransactionController', ['only' => ['index']]);
+Route::resource('sellers.products', 'Seller\SellerProductController', ['except' => ['create', 'show', 'edit']]);
+Route::resource('sellers.categories', 'Seller\SellerCategoryController', ['only' => ['index']]);
+Route::resource('sellers.buyers', 'Seller\SellerBuyerController', ['only' => ['index']]);
 
 /**
  * * Transactions
