6906dccc1e40fe09800d8f299e5c974577ac853b
rob <oh.ya.rob@gmail.com>
2018-04-18 21:11:17 -0500

    added cache

--- app/Traits/ApiResponser.php
+++ app/Traits/ApiResponser.php
@@ -3,8 +3,9 @@
 namespace App\Traits;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Collection;
 use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Validator;
 
 trait ApiResponser
@@ -18,12 +19,15 @@ trait ApiResponser
     {
         return response()->json(['error' => $message, 'code' => $code], $code);
     }
+
     protected function showAll(Collection $collection, $code = 200)
     {
         if ($collection->isEmpty()) {
             return $this->successResponse(['data' => $collection], $code);
         }
+
         $transformer = $collection->first()->transformer;
+
         $collection = $this->filterData($collection, $transformer);
         $collection = $this->sortData($collection, $transformer);
         $collection = $this->paginate($collection);
@@ -32,6 +36,7 @@ trait ApiResponser
 
         return $this->successResponse($collection, $code);
     }
+
     protected function showOne(Model $instance, $code = 200)
     {
         $transformer = $instance->transformer;
@@ -40,6 +45,7 @@ trait ApiResponser
 
         return $this->successResponse($instance, $code);
     }
+
     protected function showMessage($message, $code = 200)
     {
         return $this->successResponse(['data' => $message], $code);
@@ -54,6 +60,7 @@ trait ApiResponser
                 $collection = $collection->where($attribute, $value);
             }
         }
+
         return $collection;
     }
 
@@ -61,38 +68,59 @@ trait ApiResponser
     {
         if (request()->has('sort_by')) {
             $attribute = $transformer::originalAttribute(request()->sort_by);
+
             $collection = $collection->sortBy->{$attribute};
         }
+
         return $collection;
     }
+
     protected function paginate(Collection $collection)
     {
         $rules = [
-            'per_page' =>'integer|min:2|max:50',
+            'per_page' => 'integer|min:2|max:50',
         ];
+
         Validator::validate(request()->all(), $rules);
+
         $page = LengthAwarePaginator::resolveCurrentPage();
+
         $perPage = 15;
         if (request()->has('per_page')) {
-            $perPage = (int) request()->per_page;
+            $perPage = (int)request()->per_page;
         }
-        $results = $collection->slice(($page-1) *  $perPage, $perPage)->values();
+
+        $results = $collection->slice(($page - 1) * $perPage, $perPage)->values();
+
         $paginated = new LengthAwarePaginator($results, $collection->count(), $perPage, $page, [
             'path' => LengthAwarePaginator::resolveCurrentPath(),
         ]);
-            $paginated->appends(request()->all());
+
+        $paginated->appends(request()->all());
 
         return $paginated;
+
     }
 
     protected function transformData($data, $transformer)
     {
         $transformation = fractal($data, new $transformer);
+
         return $transformation->toArray();
     }
 
     protected function cacheResponse($data)
     {
+        $url = request()->url();
+        $queryParams = request()->query();
 
+        ksort($queryParams);
+
+        $queryString = http_build_query($queryParams);
+        $fullUrl = "{url}?{$queryString}";
+
+        return Cache::remember($fullUrl, 30 / 60, function () use ($data) {
+            return $data;
+        });
     }
 }
